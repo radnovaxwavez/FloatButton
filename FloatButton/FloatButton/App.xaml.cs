a5654a8afbd5c11162494db72b7136dc5e24c7de
oscar douglas <q_oscardouglas@outlook.com>
2023-01-26 19:22:39 +0000

    Add project files.

---
+++ FloatButton/FloatButton/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FloatButton
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Device.SetFlags(new[] { "Expander_Experimental" }); //Note how the expander is called in the App class itself, not even the MainPage where you'd think it'd be referenced
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FloatButton/FloatButton/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FloatButton
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = this; //Setting binding context to that of the MainPage class itself so all classes flow through it
+        }
+        public ObservableCollection<Album> MyImages { get; set; } //Calling on datasheet again
+    }
+
+    public class Album
+    {
+        public string Image { get; set; }
+        public string Description { get; set; }
+    }
+}
